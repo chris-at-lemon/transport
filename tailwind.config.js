53d463b310274ef028d27ec35fc60df00040da82
Chris Herrmann <cherrmann@lemon-digital.com>
2022-09-17 11:33:01 +0200

    add transport font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      transportLight: ["TransportNew-Light", "sans-serif"],
+      transportMedium: ["TransportNew-Medium", "sans-serif"],
+      transportHeavy: ["TransportNew-Heavy", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
