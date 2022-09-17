a95676184fcf3f5abda94cdd7567dba999da1069
Chris Herrmann <cherrmann@lemon-digital.com>
2022-09-17 11:41:10 +0200

    add font variations and sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,22 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mt-4">
       <div className="grid grid-cols-1">
-        <div className="text-4xl font-transportHeavy text-center">Hello Transport</div>
-        <div className="text-4xl font-transportMedium text-center">Hello Transport</div>
-        <div className="text-4xl font-transportLight text-center">Hello Transport</div>
+        <div className="text-4xl font-transportHeavy text-center mb-4">Hello Transport Heavy</div>
+        <div className="text-2xl  font-transportHeavy text-center mb-4">Hello Transport Heavy</div>
+        <div className="text-xl  font-transportHeavy text-center mb-4">Hello Transport Heavy</div>
+        <div className="text-base font-transportHeavy text-center mb-4">Hello Transport Heavy</div>
+        <hr />
+        <div className="text-4xl font-transportMedium text-center mb-4 mt-4">Hello Transport Medium</div>
+        <div className="text-2xl  font-transportMedium text-center mb-4">Hello Transport Medium</div>
+        <div className="text-xl  font-transportMedium text-center mb-4">Hello Transport Medium</div>
+        <div className="text-base font-transportMedium text-center mb-4">Hello Transport Medium</div>
+        <hr />
+        <div className="text-4xl font-transportLight text-center mb-4 mt-4">Hello Transport Light</div>
+        <div className="text-2xl  font-transportLight text-center mb-4">Hello Transport Light</div>
+        <div className="text-xl  font-transportLight text-center mb-4">Hello Transport Light</div>
+        <div className="text-base font-transportLight text-center mb-4">Hello Transport Light</div>
       </div>
     </div>
   );
